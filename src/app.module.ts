4dca59a32f47615e11f60734a4562c66a1697f8a
stats <stasafanasev21220@gmail.com>
2022-06-08 14:34:01 +0300

    удалил модель меню

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MenuModule } from './menu/menu.module';
 import { ProductModule } from './product/product.module';
 import { AuthModule } from './auth/auth.module';
 import { CartModule } from './cart/cart.module';
 import { PageModule } from './page/page.module';
+import { ReviewModule } from './review/review.module';
 
 @Module({
-  imports: [MenuModule, ProductModule, AuthModule, CartModule, PageModule],
+  imports: [ProductModule, AuthModule, CartModule, PageModule, ReviewModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -1,6 +1,53 @@
 import { Base, TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
+import { prop } from "@typegoose/typegoose";
 
-export interface ProductModel extends Base {}
-export class ProductModel extends TimeStamps{
+export class ProductCharacteristics {
+  @prop()
+  name: string;
 
+  @prop()
+  value: string;
+}
+
+export enum AvailableEnum {
+  failure,
+  success
+}
+
+export interface ProductModel extends Base {
+}
+
+export class ProductModel extends TimeStamps {
+  @prop()
+  image?: string;
+
+  @prop({unique: true})
+  title: string;
+
+  @prop()
+  description?: string; // описание
+
+  @prop()
+  price: number; //цена
+
+  @prop()
+  oldPrice?: number;
+
+  @prop({unique: true})
+  productCode: number
+
+  @prop()
+  initialRating?: number;
+
+  @prop({enum: AvailableEnum})
+  available: AvailableEnum
+
+  @prop({ type: () => [String] })
+  categories: string[];
+
+  @prop({ type: () => [ProductCharacteristics], _id: false })
+  characteristics?: ProductCharacteristics[];
+
+  @prop({ type: () => [String] })
+  tags: string[];
 }
