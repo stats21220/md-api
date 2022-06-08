11199bb81e06cb83d2f4b360818520092996df69
stats <stasafanasev21220@gmail.com>
2022-06-08 12:22:22 +0300

    создал модули, добавил моделей и начал заполнять

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MenuModule } from './menu/menu.module';
+import { ProductModule } from './product/product.module';
+import { AuthModule } from './auth/auth.module';
+import { CartModule } from './cart/cart.module';
+import { PageModule } from './page/page.module';
 
 @Module({
-  imports: [],
+  imports: [MenuModule, ProductModule, AuthModule, CartModule, PageModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/pipes/add-validation.pipe.ts
@@ -0,0 +1,18 @@
+import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from "@nestjs/common";
+import { ID_VALIDATION_ERROR } from "./add-validation.constants";
+// @ts-ignore
+import { Types } from "@types/mongoose";
+
+
+@Injectable()
+export class IdValidationPipe implements PipeTransform {
+  transform(value: string, metadata: ArgumentMetadata) {
+    if (metadata.type != "param") {
+      return value;
+    }
+    if (!Types.ObjectId.isValid(value)) {
+      throw new BadRequestException(ID_VALIDATION_ERROR);
+    }
+    return value;
+  }
+}
