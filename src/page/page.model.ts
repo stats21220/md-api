635f3432fb89236972b32045fbfdd47d7ded6f32
stats <stasafanasev21220@gmail.com>
2022-06-14 14:53:27 +0300

    страницы уникальные через масив

--- src/page/page.model.ts
+++ src/page/page.model.ts
@@ -1,29 +1,21 @@
 import { prop } from "@typegoose/typegoose";
 import { Base, TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
 
-export enum TopLevelCategoryEnum {
-  lumber,
-  buildingMaterials,
-  bathShip,
-  relatedProducts,
-  doorAndWindowBlocks,
-  paintAndVarnishMaterials
-}
 
 export interface PageModel extends Base {
 }
 
 export class PageModel extends TimeStamps {
-  @prop({ enum: TopLevelCategoryEnum, type: () => Number })
-  firstLevelCategory: TopLevelCategoryEnum;
+  @prop()
+  firstLevelCategory: string;
 
-  @prop({ unique: true })
-  secondCategory: string;
+  @prop()
+  secondCategory?: string;
 
-  @prop({ unique: true })
+  @prop()
   thirdCategory?: string;
 
-  @prop({ unique: true })
+  @prop()
   title: string;
 
   @prop()
--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -1,6 +1,7 @@
 import { Base, TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
 import { prop } from "@typegoose/typegoose";
 
+
 export class ProductCharacteristics {
   @prop()
   name: string;
@@ -21,7 +22,7 @@ export class ProductModel extends TimeStamps {
   @prop()
   image?: string;
 
-  @prop({unique: true})
+  @prop({ unique: true })
   title: string;
 
   @prop()
@@ -33,16 +34,16 @@ export class ProductModel extends TimeStamps {
   @prop()
   oldPrice?: number;
 
-  @prop({unique: true})
-  productCode: number
+  @prop({ unique: true })
+  productCode: number;
 
   @prop()
   initialRating?: number;
 
-  @prop({enum: AvailableEnum})
-  available: AvailableEnum.success // есть ли в наличии 0 нет 1 да
+  @prop({ enum: AvailableEnum })
+  available: AvailableEnum.success; // есть ли в наличии 0 нет 1 да
 
-  @prop({ type: () => [String] })
+  @prop()
   categories: string[];
 
   @prop({ type: () => [ProductCharacteristics], _id: false }) // id позволяет отключить индексы по вложеному объекту
